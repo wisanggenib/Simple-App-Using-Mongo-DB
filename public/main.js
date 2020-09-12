6251a59dc4f89e17f7ec27e5cc669ffc57489b2c
Bagus Wisanggeni <bagus.wisanggeni040897@gmail.com>
2020-09-12 12:11:28 +0700

    Update system

--- public/main.js
+++ public/main.js
@@ -1,5 +1,7 @@
 // main.js
 const update = document.querySelector('#update-button')
+const deleteButton = document.querySelector('#delete-button')
+
 
 update.addEventListener('click', _ => {
     fetch('/quotes', {
@@ -17,5 +19,23 @@ update.addEventListener('click', _ => {
         })
         .then(response => {
             window.location.reload(true)
+        })
+})
+
+deleteButton.addEventListener('click', _ => {
+    fetch('/quotes', {
+            method: 'delete',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: 'Darth Vadar'
+            })
+        })
+        .then(res => {
+            if (res.ok) return res.json()
+        })
+        .then(data => {
+            window.location.reload()
         })
 })
--- server.js
+++ server.js
@@ -60,6 +60,18 @@ MongoClient.connect(connectionString, {
                 })
                 .catch(error => console.error(error))
         })
+        app.delete('/quotes', (req, res) => {
+            quotesCollection.deleteOne({
+                    name: req.body.name
+                })
+                .then(result => {
+                    if (result.deletedCount === 0) {
+                      return res.json('No quote to delete')
+                    }
+                    res.json(`Deleted Darth Vadar's quote`)
+                  })
+                  .catch(error => console.error(error))
+        })
 
     })
     .catch(error => console.error(error))
