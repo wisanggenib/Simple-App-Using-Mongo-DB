3d1bad1f09345b6d1f8a3c93c5f5897d63cfc3c2
Bagus Wisanggeni <bagus.wisanggeni040897@gmail.com>
2020-09-12 09:49:53 +0700

    Conneting MongoDB, And Create Data

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+const app = express();
+
+app.listen(3000, function () {
+    console.log('listening on 3000')
+})
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+    // Note: __dirname is the current directory you're in. Try logging it and see what you get!
+    // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+})
+
+// app.post('/quotes', (req, res) => {
+//     console.log(req.body)
+// })
+
+
+const connectionString = 'mongodb+srv://root:passwordhere@cluster0.5zx6q.mongodb.net/test?retryWrites=true&w=majority'
+MongoClient.connect(connectionString, {
+        useUnifiedTopology: true
+    })
+    .then(client => {
+        console.log('Connected to Database')
+        const db = client.db('test')
+        const quotesCollection = db.collection('quotes')
+        app.post('/quotes', (req, res) => {
+            quotesCollection.insertOne(req.body)
+              .then(result => {
+                res.redirect('/')
+              })
+              .catch(error => console.error(error))
+          })
+    })
+    .catch(error => console.error(error))
